4bca22ff5ec8f93dfad92b60a5ff1f7f08ea32a8
Tahor Kamker <treeckozow@gmail.com>
2024-12-01 05:39:57 +0200

    bash final project, server side and client side

--- bash-project/bash-project-client.py
+++ bash-project/bash-project-client.py
@@ -1,12 +1,12 @@
 from SshToServer import SshToServer
 
-my_ssh = SshToServer(r"C:\Users\treec\Desktop\forward\AWS\keypair-1.pem", "16.16.63.19", "ubuntu")
-
 def getCommandOutput(command):
     stdout, stderr = my_ssh.runRemoteCommand(command)
     if stderr != "":
         return "Error: " + stderr
     return stdout
+
+my_ssh = SshToServer(r"C:\Users\treec\Desktop\forward\AWS\keypair-1.pem", "16.16.63.19", "ubuntu")
 
 file_name = input("Please enter the name of the file: ")
 waiting_time = input("How many seconds to wait? ")
