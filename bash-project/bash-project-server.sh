61f6153766086874d1b7b3e97121d7292f21439d
Tahor Kamker <treeckozow@gmail.com>
2024-12-03 02:59:38 +0200

    bash project, server side modified

--- bash-project/bash-project-server.sh
+++ bash-project/bash-project-server.sh
@@ -3,18 +3,14 @@
 FILE_NAME=$1
 WAITING_SECONDS=$2
 
-if [ ! -e "$FILE_NAME" ]; then
-    for sec in $(seq 1 $WAITING_SECONDS)
-    do
-        if [ -e "$FILE_NAME" ]; then
-            echo "File $FILE_NAME arrived in server after $sec seconds"
-            exit
-        fi
-        sleep 1
-        if [ $sec -eq $WAITING_SECONDS ]; then
-	    echo "Timeout"
-        fi
-    done
-else
-    echo "File $FILE_NAME exists"
-fi
+
+for sec in $(seq 1 $WAITING_SECONDS)
+do
+    if [ -e "$FILE_NAME" ]; then
+        echo "File $FILE_NAME arrived in server after $sec seconds"
+        exit
+    fi
+    sleep 1
+done
+
+echo "Timeout"
